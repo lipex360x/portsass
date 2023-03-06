2fd19d0495f20c1e3c3b15d1822f97306905ccc3
Felipe Bambirra <lipex360@gmail.com>
2023-03-06 18:46:49 -0300

    about section

---
+++ src/scripts/functions.ts
@@ -0,0 +1,15 @@
+import scrollIntoView from 'scroll-into-view'
+
+const navigations = document.querySelectorAll('.navigation')
+
+navigations.forEach((navigation) => {
+  navigation.addEventListener('click', () => {
+    const domParser = navigation as HTMLElement
+    
+    const domElement = domParser.dataset.target as string
+    
+    const element = document.getElementById(domElement)
+
+    scrollIntoView(element!, { time: 0 });
+  })
+});
